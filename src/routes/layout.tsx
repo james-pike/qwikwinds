26378c307ee769ae93d8220a1d22647dafa505be
James <jamesandrewpike@gmail.com>
2024-12-10 14:51:37 -0800

    commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,9 +3,23 @@ import { component$, Slot } from "@builder.io/qwik";
 import Footer from "~/components/widgets/Footer";
 import Header from "~/components/widgets/Header";
 
+const gridItemsHTML = new Array(30)
+  .fill(null)
+  .map(
+    () =>
+      `<div class="bg-blue-500 rounded animate-pulse" style="animation-delay: ${
+        Math.random() * 5
+      }s; animation-duration: ${3 + Math.random() * 3}s"></div>`
+  )
+  .join("");
+
 export default component$(() => {
   return (
     <>
+      <div
+        class="grid-background absolute inset-0 -z-10 grid grid-cols-6 gap-1"
+        dangerouslySetInnerHTML={gridItemsHTML}
+      ></div>
       <Header />
       <main>
         <Slot />
