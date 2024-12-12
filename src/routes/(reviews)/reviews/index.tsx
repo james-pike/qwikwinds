c1c3289ad5439febecc4b6d25b5e5d8798fdb880
James <jamesandrewpike@gmail.com>
2024-12-12 11:12:59 -0800

    commit

--- src/routes/(reviews)/reviews/index.tsx
+++ src/routes/(reviews)/reviews/index.tsx
@@ -12,6 +12,13 @@ export default component$(() => {
         title="Feedback From Our Clients"
         subtitle="Leave some information in the form below, we're passionate about your project!"
       />
+
+      <div class="w-full h-2 opacity-100"></div>
+      <Pricing
+        highlight="Client Reviews"
+        title="Feedback From Our Clients"
+        subtitle="Leave some information in the form below, we're passionate about your project!"
+      />
       
     </>
   );
