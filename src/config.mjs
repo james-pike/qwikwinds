46e19e8f54d3ea4efdac60e32b46cf1a6569ea38
James <jamesandrewpike@gmail.com>
2024-12-19 00:15:02 -0500

    commit

--- src/config.mjs
+++ src/config.mjs
@@ -2,9 +2,9 @@ export const SITE = {
   name: "Froppy",
 
   title:
-    "Froppy — Lorem Ipsum Froppy + JS to build your new website.",
+    "",
   description:
-    "Froppy is a production ready software to make your website using Javascript.",
+    "",
 
   origin: "https://froppy.xyz",
   basePathname: "/",
