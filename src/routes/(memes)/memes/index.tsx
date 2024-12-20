c0ad02634154dcc7be6f6d9d7ef0276bbd5f3632
James <jamesandrewpike@gmail.com>
2024-12-20 17:20:17 -0500

    commit

--- src/routes/(memes)/memes/index.tsx
+++ src/routes/(memes)/memes/index.tsx
@@ -14,7 +14,7 @@ export default component$(() => {
         // subtitle="Leave some information in the form below, we're passionate about your project!"
       />
        <MemeCarousel/>
-      <div class="h-2"> </div>
+    
     </>
   );
 });
