1e0188883af7fcbe7e4ee851107cc5238105947f
James <jamesandrewpike@gmail.com>
2024-12-21 13:02:49 -0500

    commit

--- src/components/widgets/MemeCarousel.tsx
+++ src/components/widgets/MemeCarousel.tsx
@@ -1,8 +1,8 @@
 import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 import { Carousel } from '@qwik-ui/headless';
+import { LuPause, LuPlay } from '@qwikest/icons/lucide';
 
 export default component$(() => {
-  // Updated array to hold image filenames
   const images = ['meme1.jpg', 'meme2.jpg', 'meme3.jpg'];
   const isPlaying = useSignal<boolean>(false);
 
@@ -13,50 +13,41 @@ export default component$(() => {
   return (
     <>
       <Carousel.Root
-        class="carousel-root px-2 pt-2"
+        class="carousel-root px-1 pt-1"
         gap={30}
         autoPlayIntervalMs={3500}
         bind:autoplay={isPlaying}
       >
-        {/* <div class="carousel-buttons">
-          <Carousel.Previous>Prev</Carousel.Previous>
-          <Carousel.Player>{isPlaying.value ? <LuPause /> : <LuPlay />}</Carousel.Player>
-          <Carousel.Next>Next</Carousel.Next>
-        </div> */}
-     
-          {images.map((image, index) => (
-            <Carousel.Slide key={image} class="carousel-slide">
-              {/* Replace color with img for the images */}
-              <img src={`/images/${image}`} alt={`Meme ${index + 1}`} />
-              {/* <div>{index === 1 && <button>I stop autoplay on focus!</button>}</div> */}
-            </Carousel.Slide>
-          ))}
-      
+        {/* Flex container for buttons */}
+    
+
+        {/* Carousel slides */}
+        {images.map((image, index) => (
+          <Carousel.Slide key={image} class="carousel-slide">
+            <img src={`/images/${image}`} alt={`Meme ${index + 1}`} />
+          </Carousel.Slide>
+        ))}
+
+<div class="carousel-buttons-container flex justify-between items-center w-full">
+          {/* Left side: Carousel controls */}
+          <div class="flex gap-4">
+            <Carousel.Previous class="carousel-control">Prev</Carousel.Previous>
+            <Carousel.Player class="carousel-control">
+              {isPlaying.value ? <LuPause /> : <LuPlay />}
+            </Carousel.Player>
+            <Carousel.Next class="carousel-control">Next</Carousel.Next>
+          </div>
+
+          {/* Right side: Social share buttons */}
+          <div class="social-share-buttons flex gap-4">
+            <button class="social-button">Facebook</button>
+            <button class="social-button">Twitter</button>
+            <button class="social-button">Instagram</button>
+          </div>
+        </div>
+
+
       </Carousel.Root>
-      {/* <p>isPlaying: {isPlaying.value.toString()}</p>
-      <button onClick$={() => (isPlaying.value = !isPlaying.value)}>
-        Toggle autoplay
-      </button> */}
     </>
   );
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
